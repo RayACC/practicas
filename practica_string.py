79e8cd577d28286c4314aed8c1e53876bdcbb238
RayACC <rayccoriakun@gmail.com>
2018-03-27 21:19:23 -0700

    Practica listas con while

---
+++ practica_string.py
@@ -0,0 +1,9 @@
+mi_lista = ["hola","como","estas","hoy"]
+longitu=len(mi_lista)
+indice = 0
+
+while longitu > indice:
+    print("Esto contiene la string {} ".format(mi_lista[indice]))
+    indice += 1
+
+print("final de la lista")
