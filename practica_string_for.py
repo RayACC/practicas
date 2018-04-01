3c1234856aaf4e5a25dd44dc3241e0cdd793437f
RayACC <rayccoriakun@gmail.com>
2018-03-31 17:11:48 -0700

    debug

--- practica_string_for.py
+++ practica_string_for.py
@@ -1,21 +1,22 @@
 vocal = ["a","e","i","o","u"]
 salir=""
-while fin != "fin" 
-  frase = input("introduce tu frase")
+
+while salir != "fin":
+  frase = input("introduce tu frase:     ")
  
   vocales = 0
   consonantes = 0
 
   for x in frase:
   
-    if x in vocal 
+    if x in vocal:
         vocales += 1
-    else
+    else:
         consonantes += 1
 
-  print("Numero de vocales:  {} ".format(vocales))  
+  print("Numero de vocales: {} ".format(vocales))
   print("Numero de consonantes: {}".format(consonantes))
-  salir=input("Si deseas terminar escribe fin:")
+  salir=input("Si deseas terminar escribe fin:   ")
   salir= salir.lower()
   
 print("fin del programa ")
